8a0b7b6f013504bfe9fbaf7679ac4d462a833dda
Pavel Mushkaterov <pavel.mush@gmail.com>
2024-01-09 03:08:21 +0300

    Update src/index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import { program } from 'commander';
 const genDiff = () => {
     program
         .description('Compares two configuration files and shows a difference.')
-        .version('1.0.0');
+        .version('1.0.0')
+        .arguments('<filepath1> <filepath2>')
+        .option('-f, --format <type>', 'output format', 'stylish');
+
     program.parse();
 };
 
